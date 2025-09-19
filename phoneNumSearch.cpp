f8e22520be9d9d2c5574b8a9ebfa210abe476e0f
MichaelMulieri <mulierim@merrimack.edu>
2025-09-19 18:26:45 -0400

    Initial commit

--- phoneNumSearch.cpp
+++ phoneNumSearch.cpp
@@ -1,4 +1,3 @@
-// 
 
 #include <iostream>
 #include <fstream>
@@ -9,33 +8,36 @@
 #include <cctype>
 using namespace std;
 
-
+// function to calculate the sum of digits in a phone number
 int getSum(long long phoneNumber) {
     int sum = 0;
     while (phoneNumber > 0) {
+        // add last digit to the sum and remove it from the number
         sum += phoneNumber % 10;
         phoneNumber /= 10;
     }
     return sum;
     
 }
-
+// function to check if a phone number matches the user's input
 bool checkForMatch(long long userInputPhoneNumber, long long phoneNumber){
     string userInput = to_string(userInputPhoneNumber);
     string csvNumber = to_string(phoneNumber);
 
+    // check if the lengths are different
     if (userInput.length() != csvNumber.length()) {
         return false;
     }
 
+    // check if all digits match
     for (char userDigit : userInput) {
         if (userDigit != csvNumber[0]) {
             return false;
         }
-        csvNumber.erase(0, 1);
+        csvNumber.erase(0, 1);  // remove the first digit from csvNumber
     }
 
-    return true;
+    return true; // all digits match
 }
 
 int main() {
@@ -43,6 +45,7 @@ int main() {
     cout << "Enter a phone number (10 digits): ";
     cin >> userInputPhoneNumber;
 
+    // calculate the sum of digits in the user's input number
     int userInputNumberSum = getSum(userInputPhoneNumber);
 
     // Open the CSV file
@@ -54,6 +57,7 @@ int main() {
 
     string line;
 
+    // skip the header line (csv file format)
     getline(inputFile, line);
 
     while (getline(inputFile, line)) {
@@ -61,9 +65,12 @@ int main() {
         string phoneNumberStr;
         int timesCalled;
 
+        // get the phone number and the number of times it was called from the CSV file
         if (getline(iss, phoneNumberStr, ',') && (iss >> timesCalled)){
         long long phoneNumber = stoll(phoneNumberStr);
         int phoneNumberSum = getSum(phoneNumber);
+        // check if the sum of digits in the phone number matches the sum of digits in the user's input number,
+        // and if the phone number matches the user's input number itself.  If so, display the phone number and its call count.
         if (phoneNumberSum == userInputNumberSum) {
             bool isMatch = checkForMatch(userInputPhoneNumber, phoneNumber);
             if (isMatch){
@@ -75,6 +82,7 @@ int main() {
         }
         }
     }
+    // if no match found
     cout << "Phone number not found." << endl;
     inputFile.close();
     return 0;
